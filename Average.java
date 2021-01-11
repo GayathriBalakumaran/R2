9ef87756ccb0255dcf34e7f77dcf3c666f5a072b
GayathriBalakumaran <77260500+GayathriBalakumaran@users.noreply.github.com>
2021-01-11 19:55:51 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Average{
+  public static void main(String []args){
+    int r1,r2,s;
+    Scanner kbd=new Scanner(System.in);
+    r1=kbd.nextInt();
+    s=kbd.nextInt();
+    int r=s*2;
+    r2=r-r1;
+    System.out.print(r2);
+  }
+}
